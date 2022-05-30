ba5170e1865ac6d7935f07d6ff22e065683713cf
Your Name <you@example.com>
2022-05-30 19:11:55 +0300

    remove black belt

--- functions/postEpisodeInChannel.js
+++ functions/postEpisodeInChannel.js
@@ -55,9 +55,9 @@ module.exports = (bot, dt, anyErr) => {
                             extraParams = 'NK'
                         }
 
-                        let cap = `<b>Ep. ${noEp.substring(1)} | ${capQty}  \n${muxed}\n▬▬▬▬▬▬▬▬▬▬▬▬\n⭐️ More Telegram K-Drama WWW.DRAMASTORE.NET</b>`
+                        let cap = `<b>Ep. ${noEp.substring(1)} | ${capQty}  \n${muxed}\n\n⭐️ More Telegram K-Drama WWW.DRAMASTORE.NET</b>`
                         if(muxed == '#English Soft-subbed') {
-                            cap = `<b>Ep. ${noEp.substring(1)} | ${capQty}  \n${muxed}\n▬▬▬▬▬▬▬▬▬▬▬▬\n⭐️ More Telegram K-Drama WWW.DRAMASTORE.NET</b>\n\n<i>- This ep. is soft-subbed, use VLC or MX Player to see subtitles</i>`
+                            cap = `<b>Ep. ${noEp.substring(1)} | ${capQty}  \n${muxed}\n\n⭐️ More Telegram K-Drama WWW.DRAMASTORE.NET</b>\n\n<i>- This ep. is soft-subbed, use VLC or MX Player to see subtitles</i>`
                         }
 
                         await bot.telegram.editMessageCaption(ctx.channelPost.chat.id, msgId, '', cap, { parse_mode: 'HTML' })
