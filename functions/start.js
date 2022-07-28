de23944d1f1270f33d80ba6cd11c0955f94da7a7
gshemdoe <georgehmariki@gmail.com>
2022-07-29 02:09:18 +0300

    blocked false

--- functions/start.js
+++ functions/start.js
@@ -63,91 +63,93 @@ module.exports = (bot, dt, anyErr) => {
                     }
                 }
 
-                let epMsgId = startPayload.split('shemdoe')[1].trim()
-                let fontUrl = `https://font5.net/blog/post.html?id=62cd8fbe9de0786aafdb98b7#adding-points-dramastore-userid=DS${ctx.chat.id}`
-                let url = `http://www.dramastore.net/user/${ctx.chat.id}/boost`
+                else {
+                    let epMsgId = startPayload.split('shemdoe')[1].trim()
+                    let fontUrl = `https://font5.net/blog/post.html?id=62cd8fbe9de0786aafdb98b7#adding-points-dramastore-userid=DS${ctx.chat.id}`
+                    let url = `http://www.dramastore.net/user/${ctx.chat.id}/boost`
 
 
-                let ptsKeybd = [
-                    { text: '🥇 My Points', callback_data: 'mypoints' },
-                    { text: '➕ Add points', url: fontUrl }
-                ]
-                let closeKybd = [
-                    { text: '👌 Ok, I understand', callback_data: 'closePtsMsg' }
-                ]
+                    let ptsKeybd = [
+                        { text: '🥇 My Points', callback_data: 'mypoints' },
+                        { text: '➕ Add points', url: fontUrl }
+                    ]
+                    let closeKybd = [
+                        { text: '👌 Ok, I understand', callback_data: 'closePtsMsg' }
+                    ]
 
-                // add user to database
-                let user = await usersModel.findOne({ userId: ctx.chat.id })
-                if (!user) {
-                    let newUser = await usersModel.create({
-                        userId: ctx.chat.id,
-                        points: 10,
-                        fname: ctx.chat.first_name,
-                        downloaded: 1,
-                        blocked: false
-                    })
+                    // add user to database
+                    let user = await usersModel.findOne({ userId: ctx.chat.id })
+                    if (!user) {
+                        let newUser = await usersModel.create({
+                            userId: ctx.chat.id,
+                            points: 10,
+                            fname: ctx.chat.first_name,
+                            downloaded: 1,
+                            blocked: false
+                        })
 
-                    let txt = ''
+                        let txt = ''
 
-                    if (ctx.chat.id == dt.naomy) {
-                        txt = `Karibu <b>Zumaridi</b> 😊 bestie yake shemdoe\nUmepata points 10 za bure.\n- Kila episode nitakayokupa nitakukata points 2.\n- Ili nikutumie episode unatakiwa uwe na angalau points 2, chini ya hapo itakupasa kuziongeza kwa kubofya button ya "➕ Add Points."`
-                    }
-                    else if (ctx.chat.id == dt.jacky) {
-                        txt = `Karibu <b>Jacky</b> 😊 rafiki yake shemdoe\nUmepata points 10 za bure.\n- Kila episode nitakayokupa nitakukata points 2.\n- Pia ili nikutumie episode unatakiwa uwe na angalau points 2, chini ya hapo itakupasa kuziongeza kwa kubofya button ya "➕ Add Points."`
-                    } else {
-                        txt = `Welcome <b>${ctx.chat.first_name}</b>, this is Drama Store Bot, you were provided with 10 free points\n\n- To get episode file you need to have at least 2 points.\n- Each episode you get will cost you 2 points.\n- You can easily add your points through the "➕ Add points" button`
-                    }
-                    await ctx.reply(txt, { parse_mode: 'HTML' })
-                    bot.telegram.copyMessage(ctx.chat.id, dt.databaseChannel, epMsgId, {
-                        reply_markup: {
-                            inline_keyboard: [ptsKeybd]
+                        if (ctx.chat.id == dt.naomy) {
+                            txt = `Karibu <b>Zumaridi</b> 😊 bestie yake shemdoe\nUmepata points 10 za bure.\n- Kila episode nitakayokupa nitakukata points 2.\n- Ili nikutumie episode unatakiwa uwe na angalau points 2, chini ya hapo itakupasa kuziongeza kwa kubofya button ya "➕ Add Points."`
                         }
-                    })
-
-                    await newUser.updateOne({ $inc: { points: -pt } })
-                    setTimeout(() => {
-                        ctx.reply(`You got the file and ${pt} point(s) deducted from your points balance.\n\nYou remain with <b>${newUser.points - pt} points</b>`, {
-                            parse_mode: 'HTML',
+                        else if (ctx.chat.id == dt.jacky) {
+                            txt = `Karibu <b>Jacky</b> 😊 rafiki yake shemdoe\nUmepata points 10 za bure.\n- Kila episode nitakayokupa nitakukata points 2.\n- Pia ili nikutumie episode unatakiwa uwe na angalau points 2, chini ya hapo itakupasa kuziongeza kwa kubofya button ya "➕ Add Points."`
+                        } else {
+                            txt = `Welcome <b>${ctx.chat.first_name}</b>, this is Drama Store Bot, you were provided with 10 free points\n\n- To get episode file you need to have at least 2 points.\n- Each episode you get will cost you 2 points.\n- You can easily add your points through the "➕ Add points" button`
+                        }
+                        await ctx.reply(txt, { parse_mode: 'HTML' })
+                        bot.telegram.copyMessage(ctx.chat.id, dt.databaseChannel, epMsgId, {
                             reply_markup: {
-                                inline_keyboard: [closeKybd]
+                                inline_keyboard: [ptsKeybd]
                             }
                         })
-                    }, 1000)
 
-                }
-                if (user && user.points >= 2) {
-                    await bot.telegram.copyMessage(ctx.chat.id, dt.databaseChannel, epMsgId, {
-                        reply_markup: {
-                            inline_keyboard: [ptsKeybd]
-                        }
-                    })
+                        await newUser.updateOne({ $inc: { points: -pt } })
+                        setTimeout(() => {
+                            ctx.reply(`You got the file and ${pt} point(s) deducted from your points balance.\n\nYou remain with <b>${newUser.points - pt} points</b>`, {
+                                parse_mode: 'HTML',
+                                reply_markup: {
+                                    inline_keyboard: [closeKybd]
+                                }
+                            })
+                        }, 1000)
 
-                    await user.updateOne({ $inc: { points: -pt, downloaded: 1 } })
-                    console.log(`Episode sent to ${user.fname}`)
-
-                    setTimeout(() => {
-                        ctx.reply(`You got the file and ${pt} point(s) deducted from your points balance.\n\nYou remain with <b>${user.points - pt} points.</b>`, {
-                            parse_mode: 'HTML'
-                        }).then((yohave) => {
-                            setTimeout(() => {
-                                bot.telegram.deleteMessage(ctx.chat.id, yohave.message_id)
-                                    .catch((err) => {
-                                        if (err.description.includes('delete')) {
-                                            bot.telegram.sendMessage(dt.shd, err.description)
-                                        }
-                                    })
-                            }, 10000)
+                    }
+                    if (user && user.points >= 2) {
+                        await bot.telegram.copyMessage(ctx.chat.id, dt.databaseChannel, epMsgId, {
+                            reply_markup: {
+                                inline_keyboard: [ptsKeybd]
+                            }
                         })
-                    }, 1000)
 
-                }
-                if (user && user.points < 2) {
-                    await ctx.reply(`You don't have enough points to get the file (you need at least 2 points)\n\nYou have <b>${user.points}</b> points... Click <b>"➕ Add points"</b> button below to increase your points, alternatively you can follow this link ${url}`, {
-                        parse_mode: 'HTML',
-                        reply_markup: {
-                            inline_keyboard: [ptsKeybd]
-                        }
-                    })
+                        await user.updateOne({ $inc: { points: -pt, downloaded: 1 } })
+                        console.log(`Episode sent to ${user.fname}`)
+
+                        setTimeout(() => {
+                            ctx.reply(`You got the file and ${pt} point(s) deducted from your points balance.\n\nYou remain with <b>${user.points - pt} points.</b>`, {
+                                parse_mode: 'HTML'
+                            }).then((yohave) => {
+                                setTimeout(() => {
+                                    bot.telegram.deleteMessage(ctx.chat.id, yohave.message_id)
+                                        .catch((err) => {
+                                            if (err.description.includes('delete')) {
+                                                bot.telegram.sendMessage(dt.shd, err.description)
+                                            }
+                                        })
+                                }, 10000)
+                            })
+                        }, 1000)
+
+                    }
+                    if (user && user.points < 2) {
+                        await ctx.reply(`You don't have enough points to get the file (you need at least 2 points)\n\nYou have <b>${user.points}</b> points... Click <b>"➕ Add points"</b> button below to increase your points, alternatively you can follow this link ${url}`, {
+                            parse_mode: 'HTML',
+                            reply_markup: {
+                                inline_keyboard: [ptsKeybd]
+                            }
+                        })
+                    }
                 }
             }
 
