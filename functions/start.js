7aeb4d6c1eb5062bea633c3cebc129dc1af08ec1
gshemdoe <georgehmariki@gmail.com>
2023-05-12 23:26:34 +0300

    updating usernames

--- functions/start.js
+++ functions/start.js
@@ -68,6 +68,9 @@ module.exports = (bot, dt, anyErr) => {
                     let user = await usersModel.findOne({userId: ctx.chat.id})
                     if(!user) {
                         let fname = ctx.chat.first_name
+                        if(ctx.chat.username) {
+                            fname = '@'+ctx.chat.username
+                        }
                         let blocked = false
                         let country = {name: 'unknown', c_code: 'unknown'}
                         let userId = ctx.chat.id
@@ -75,6 +78,16 @@ module.exports = (bot, dt, anyErr) => {
                         let downloaded = 0
                         await usersModel.create({fname, blocked, country, userId, points, downloaded})
                         console.log('new user from offer added')
+                    } else {
+                        if(ctx.chat.username) {
+                            if(user.fname != `@${ctx.chat.username}`) {
+                                await usersModel.findOneAndUpdate({userId: ctx.chat.id}, {$set: {fname: `@${ctx.chat.username}`}})
+                            }
+                        } else {
+                            if(user.fname != ctx.chat.first_name) {
+                                await usersModel.findOneAndUpdate({userId: ctx.chat.id}, {$set: {fname: ctx.chat.first_name}})
+                            }
+                        }
                     }
                 }
 
